9c858f4b3898f734409b75e72f08fb5eeb4992a9
bennygoldman <goldmanb@gmail.com>
2023-11-06 14:06:36 -0500

    Add fetch post route to index. Refactor functions.

--- scripts/index.js
+++ scripts/index.js
@@ -1,221 +1,244 @@
-// STATIC COMMENTS FOR TESTING
-// let commentArray = [
-//     {
-//         name: `Pandy Fackler`,
-//         date: `03/17/2023`,
-//         comment: `I was almost in a coma from doing the Bossa Nova. And the Funky Cold Medina.`
-//     },
-//     {
-//         name: `Eddie Dingell`,
-//         date: `12/25/2022`,
-//         comment: `Won't you tell me what's on your mind? If you'd open your head tell me what would I find. Are you for real? What do you feel?`
-//     },
-//     {
-//         name: `The Stallion`,
-//         date: `04/04/2020`,
-//         comment: `1. I can drink. 2. I get groomed. 3. I go for a walk. I am The Stallion.`
-//     },
-// ];
+// DOM ACTIONS
+const commentActionFactory = () => {
 
-// GET DOM ELEMENTS
-const $commentForm = document.getElementById('commentForm');
-const $commentDisplay = document.getElementById('commentDisplay');
-
-// REMOVES ERROR CLASS FROM AN ELEMENT
-const clearErrorClass = (el) => {
-    if (el.classList.contains('error')) {
-        el.classList.remove('error');
-    }
-};
-
-// ADDS ERROR CLASS TO AN ELEMENT
-const addErrorClass = (el) => {
-    el.classList.add('error');
-};
-
-// CONVERTS {MS SINCE EPOCH} DATE INTO SPEC'D FORMAT
-const convertTimestampToDate = (timestamp) => {
-    const date = timestamp ? new Date(timestamp) : new Date();
-    const format = { year: 'numeric', month: '2-digit', day: '2-digit' };
-    const formattedDate = date.toLocaleDateString('en-US', format);
-    return formattedDate;
-};
-
-// ADDS COMMENT FROM INPUT FORM TO OUTPUT AREA
-const submitCommentEvent = (e) => {
-    e.preventDefault();
-
-    const nameInput = document.getElementById('nameInput');
-    const commentInput = document.getElementById('commentInput');
-
-    const nameInputVal = nameInput.value;
-    const commentInputVal = commentInput.value;
-
-    clearErrorClass(nameInput);
-    clearErrorClass(commentInput);
-
-    if (!nameInputVal) {
-        addErrorClass(nameInput);
-    }
-
-    if (!commentInputVal) {
-        addErrorClass(commentInput);
-    }
-    if (nameInputVal && commentInputVal) {
-        const comment = createComment(nameInputVal, commentInputVal);
-        commentArray.unshift(comment);
-        renderComments(commentArray);
-
-        e.target.reset();
-    }
-};
-
-const createNewElement = (el, classNames, text) => {
-    const $el = document.createElement(el);
-    if (Array.isArray(classNames)) {
-        classNames.forEach(className => $el.classList.add(className));
-    } else {
-        classNames && ($el.classList.add(classNames));
-    }
-    text && ($el.textContent = text);
-    return $el;
-};
-
-// RENDERS ALL COMMENTS IN ARRAY TO DOM
-const renderComments = (arr) => {
-    $commentDisplay && ($commentDisplay.innerHTML = '');
-    arr.forEach(comment => {
-        const name = comment.name;
-        // const date = comment.date;
-        const date = convertTimestampToDate(comment.timestamp);
-        const id = comment.id;
-        const content = comment.comment;
-        const likes = comment.likes;
-
-        createDomElement(name, date, content, id, likes);
-    });
-};
-
-// CREATES A COMMENT OBJECT FOR ADDING TO AN ARRAY
-const createComment = (name, comment) => {
-
-    // const date = convertTimestampToDate();
-
-    return {
-        name: name,
-        comment: comment,
-        // date: date,
+    // CLEARS THE COMMENTS FROM THE DOM
+    const clearComments = () => {
+        const $commentDisplay = document.getElementById('commentDisplay');
+        $commentDisplay && ($commentDisplay.innerHTML = '');
     };
 
-};
-
-// CREATES A <LI> ELEMENT FROM AN ARRAY ITEM AND RENDERS IT TO THE DOM
-const createDomElement = (name, date, comment, id, likes) => {
-
-    const $listEl = createNewElement('li');
-    const $articleEl = createNewElement('article');
-    const $asideEl = createNewElement('aside');
-    const $aviEl = createNewElement('img', 'avatar');
-    const $divEl = createNewElement('div');
-    const $topDivEl = createNewElement('div');
-    const $midDivEl = createNewElement('div');
-    const $btmDivEl = createNewElement('div');
-    const $nameEl = createNewElement('p', ['txt', 'comment', 'name'], name);
-    const $dateEl = createNewElement('time', ['txt', 'comment', 'date'], date);
-    const $commentEl = createNewElement('p', ['txt', 'comment', 'copy'], comment);
-    // const $likeBtnEl = createNewElement('button', '', 'like');
-    // const $likeBtnEl = createNewElement('button');
-    const $likeBtnEl = createNewElement('input');
-    $likeBtnEl.setAttribute('type', 'image');
-    $likeBtnEl.setAttribute('alt', 'like');
-    $likeBtnEl.setAttribute('data-id', id ? id : null);
-    $likeBtnEl.src = '../assets/icons/svg/icon-like.svg';
-
-    // const $likeImgEl = createNewElement('img');
-
-    // $likeBtnEl.appendChild($likeImgEl);
-
-    const $likeCounterEl = createNewElement('p', ['txt', 'comment', 'label'], likes && likes === 1 ? `${likes} like` : `${likes} likes`);
-
-    // const $delBtnEl = createNewElement('button', '', 'delete');
-    // const $delBtnEl = createNewElement('button');
-    const $delBtnEl = createNewElement('input');
-    $delBtnEl.setAttribute('type', 'image');
-    $delBtnEl.setAttribute('alt', 'delete');
-    $delBtnEl.setAttribute('data-id', id ? id : null);
-
-    // const $delImgEl = createNewElement('img');
-    $delBtnEl.src = '../assets/icons/svg/icon-delete.svg';
-
-    // $delBtnEl.appendChild($delImgEl);
-
-    $aviEl.src = '../assets/images/0-boognish-avi.png';
-
-    $asideEl.appendChild($aviEl);
-
-    $topDivEl.appendChild($nameEl);
-    $topDivEl.appendChild($dateEl);
-
-    $midDivEl.appendChild($commentEl);
-
-    $btmDivEl.appendChild($likeBtnEl);
-    $btmDivEl.appendChild($likeCounterEl);
-    $btmDivEl.appendChild($delBtnEl);
-
-    $divEl.appendChild($topDivEl);
-    $divEl.appendChild($midDivEl);
-    $divEl.appendChild($btmDivEl);
-
-    $articleEl.appendChild($asideEl);
-    $articleEl.appendChild($divEl);
-
-    $listEl.appendChild($articleEl);
-
-    $commentDisplay.appendChild($listEl);
-
-};
-
-$commentForm.addEventListener('submit', submitCommentEvent);
-
-
-// API DETAILS FOR FETCH REQUEST
-const apiUrl = `https://project-1-api.herokuapp.com`;
-const jsonBinUrl = `https://api.jsonbin.io/v3/b/6541e0f912a5d37659934004`;
-const jsonBinApiKey = `$2a$10$fW/LUrdAWOy3flNyjCDoFOFMlUyRa.fcxMsLZHlgL4Tl/S5ptSUNK`;
-const jsonBinHeaders = { "X-Master-Key": `${jsonBinApiKey}` };
-
-const queryParams = `?api_key=`;
-const apiKey = `1636ce99-0ac4-46f6-b625-cac457fb121f`;
-const routeComments = `${apiUrl}/comments${queryParams}${apiKey}`;
-const postDataTest = {
-    name: 'Benny G',
-    comment: 'Ween da Best Eva'
-};
-
-async function fetchData(apiUrl) {
-    try {
-        const response = await fetch(apiUrl, {
-            method: "GET",
-            headers: jsonBinHeaders
-        });
-        if (response.ok) {
-            const data = await response.json();
-            renderComments(data.record.comments);
+    // CREATES AN ELEMENT OF ANY TYPE WITH OPTIONAL CLASSES AND INNER TEXT
+    const createNewElement = (el, classNames, text) => {
+        const $el = document.createElement(el);
+        if (Array.isArray(classNames)) {
+            classNames.forEach(className => $el.classList.add(className));
         } else {
-            console.log('NOT OK!');
+            classNames && ($el.classList.add(classNames));
         }
-    } catch (error) {
-        console.error('Error: ', error);
-        throw error;
-    }
-}
+        text && ($el.textContent = text);
+        return $el;
+    };
 
-// renderComments(commentArray);
+    // CREATES A <LI> ELEMENT FROM AN ARRAY ITEM AND RENDERS IT TO THE DOM
+    const createCommentListItem = (name, date, comment, id, likes) => {
 
-function initialLoad(apiUrl = routeComments) {
-    fetchData(apiUrl);
-}
+        const $listEl = createNewElement('li');
+        const $articleEl = createNewElement('article');
+        const $asideEl = createNewElement('aside');
+        const $aviEl = createNewElement('img', 'avatar');
+        const $divEl = createNewElement('div');
+        const $topDivEl = createNewElement('div');
+        const $midDivEl = createNewElement('div');
+        const $btmDivEl = createNewElement('div');
+        const $nameEl = createNewElement('p', ['txt', 'comment', 'name'], name);
+        const $dateEl = createNewElement('time', ['txt', 'comment', 'date'], date);
+        const $commentEl = createNewElement('p', ['txt', 'comment', 'copy'], comment);
+        const $likeBtnEl = createNewElement('input');
+        $likeBtnEl.setAttribute('type', 'image');
+        $likeBtnEl.setAttribute('alt', 'like');
+        $likeBtnEl.setAttribute('data-id', id ? id : null);
+        $likeBtnEl.src = '../assets/icons/svg/icon-like.svg';
 
-initialLoad(jsonBinUrl);
+        const $likeCounterEl = createNewElement('p', ['txt', 'comment', 'label'], likes && likes === 1 ? `${likes} like` : `${likes} likes`);
+
+        const $delBtnEl = createNewElement('input');
+        $delBtnEl.setAttribute('type', 'image');
+        $delBtnEl.setAttribute('alt', 'delete');
+        $delBtnEl.setAttribute('data-id', id ? id : null);
+
+        $delBtnEl.src = '../assets/icons/svg/icon-delete.svg';
+
+        $aviEl.src = '../assets/images/0-boognish-avi.png';
+
+        $asideEl.appendChild($aviEl);
+
+        $topDivEl.appendChild($nameEl);
+        $topDivEl.appendChild($dateEl);
+
+        $midDivEl.appendChild($commentEl);
+
+        $btmDivEl.appendChild($likeBtnEl);
+        $btmDivEl.appendChild($likeCounterEl);
+        $btmDivEl.appendChild($delBtnEl);
+
+        $divEl.appendChild($topDivEl);
+        $divEl.appendChild($midDivEl);
+        $divEl.appendChild($btmDivEl);
+
+        $articleEl.appendChild($asideEl);
+        $articleEl.appendChild($divEl);
+
+        $listEl.appendChild($articleEl);
+
+        return $listEl;
+
+    };
+
+    // CONVERTS {MS SINCE EPOCH} DATE INTO SPEC'D FORMAT
+    const convertTimestampToDate = timestamp => {
+        const date = timestamp ? new Date(timestamp) : new Date();
+        const format = { year: 'numeric', month: '2-digit', day: '2-digit' };
+        const formattedDate = date.toLocaleDateString('en-US', format);
+        return formattedDate;
+    };
+
+    // FORMATS COMMENT OBJS FOR RENDERING TO DOM
+    const renderComments = (arr) => {
+        clearComments();
+
+        arr.forEach(comment => {
+            const name = comment.name;
+            const date = convertTimestampToDate(comment.timestamp);
+            const id = comment.id;
+            const content = comment.comment;
+            const likes = comment.likes;
+            const $listItemEl = createCommentListItem(name, date, content, id, likes);
+            const $commentDisplay = document.getElementById('commentDisplay');
+            $commentDisplay.appendChild($listItemEl);
+        });
+
+    };
+
+    return { renderComments };
+};
+
+// FETCH ACTIONS
+const fetchActionFactory = () => {
+    const commentsBaseUrl = `https://project-1-api.herokuapp.com/comments`;
+    const apiKey = `?api_key=1636ce99-0ac4-46f6-b625-cac457fb121f`;
+    let id = ``;
+    let action = ``;
+    const allCommentsRoute = `${commentsBaseUrl}${apiKey}`;
+    const singleCommentRoute = `${commentsBaseUrl}${id}${action}${apiKey}`;
+
+    const getComments = async () => {
+        let data;
+        try {
+            const response = await fetch(allCommentsRoute);
+            if (response.ok) {
+                data = await response.json();
+                // console.log(data);
+                const domMethods = commentActionFactory();
+                domMethods.renderComments(data);
+            }
+            else { console.log('NOT OK!'); }
+        } catch (error) {
+            console.error('Error fetching comments:', error);
+        }
+        return data;
+    };
+
+    const postComment = async comment => {
+        let data;
+        try {
+            const headersList = {
+                "Content-Type": "application/json"
+            };
+
+            const bodyContent = JSON.stringify({
+                "name": comment.name,
+                "comment": comment.comment
+            });
+
+            const response = await fetch(allCommentsRoute, {
+                method: "POST",
+                body: bodyContent,
+                headers: headersList
+            });
+            if (response.ok) {
+                data = await response.json();
+                console.log(data);
+                getComments();
+            }
+        } catch (error) {
+            console.error('Error posting comment:', error);
+        }
+    };
+
+    const likeComment = async comment => {
+        try {
+            id = comment.id;
+            action = `like`;
+
+            let response = await fetch(singleCommentRoute, {
+                method: "PUT",
+            });
+
+            let data = await response.json();
+            console.log(data);
+            getComments();
+        } catch (error) {
+            console.error('Error liking comment:', error);
+        }
+    };
+
+    const deleteComment = async comment => {
+        try {
+            id = comment.id;
+            let response = await fetch(singleCommentRoute, {
+                method: "DELETE",
+            });
+
+            let data = await response.json();
+            console.log(data);
+            getComments();
+        } catch (error) {
+            console.error('Error deleting comment:', error);
+        }
+    };
+
+    return { getComments, postComment, likeComment, deleteComment };
+};
+
+// FORM ACTIONS
+const initalizeForm = () => {
+    const $commentForm = document.getElementById('commentForm');
+
+    // REMOVES ERROR CLASS FROM AN ELEMENT
+    const clearErrorClass = (el) => {
+        if (el.classList.contains('error')) {
+            el.classList.remove('error');
+        }
+    };
+
+    // ADDS ERROR CLASS TO AN ELEMENT
+    const addErrorClass = (el) => {
+        el.classList.add('error');
+    };
 
 
+    const submitCommentEvent = (e) => {
+        e.preventDefault();
+        const name = e.target.nameInput.value;
+        const comment = e.target.commentInput.value;
+        console.log(name, comment);
+
+        if (name && comment) {
+            const commentApi = fetchActionFactory();
+            // CREATES A COMMENT OBJECT FOR ADDING TO AN ARRAY
+            const createCommentObj = (name, comment) => {
+
+                return {
+                    name: name,
+                    comment: comment
+                };
+
+            };
+            const commentObj = createCommentObj(name, comment);
+            commentApi.postComment(commentObj);
+            e.target.reset();
+        }
+    };
+
+    $commentForm.addEventListener('submit', submitCommentEvent);
+
+};
+
+const loadPage = () => {
+    // VARIABLES FOR DOM ELEMENTS
+    const commentsApi = fetchActionFactory();
+    commentsApi.getComments();
+    initalizeForm();
+};
+
+loadPage();
--- scripts/shows.js
+++ scripts/shows.js
@@ -1,124 +1,93 @@
-// STATIC SHOWS FOR TESTING
-// let showArray = [
-//     {
-//         date: `Mon Sept 06 2021`,
-//         place: `Ronald Lane`,
-//         location: `San Francisco, CA`,
-//     },
-//     {
-//         date: `Tue Sept 21 2021`,
-//         place: `Pier 3 East`,
-//         location: `San Francisco, CA`,
-//     },
-//     {
-//         date: `Fri Oct 15 2021`,
-//         place: `View Lounge`,
-//         location: `San Francisco, CA`,
-//     },
-//     {
-//         date: `Sat Nov 06 2021`,
-//         place: `Hyatt Agency `,
-//         location: `San Francisco, CA`,
-//     },
-//     {
-//         date: `Fri Nov 26 2021`,
-//         place: `Moscow Center`,
-//         location: `San Francisco, CA`,
-//     },
-//     {
-//         date: `Wed Dec 15 2021`,
-//         place: `Press Club`,
-//         location: `San Francisco, CA`,
-//     },
-// ];
-
-const $showListEl = document.querySelector('.show-list');
-
-const createNewElement = (el, classNames, text) => {
-    const $el = document.createElement(el);
-    if (Array.isArray(classNames)) {
-        classNames.forEach(className => $el.classList.add(className));
-    } else {
-        classNames && ($el.classList.add(classNames));
-    }
-    text && ($el.textContent = text);
-    return $el;
-};
-
-const createButton = () => {
-    const button = document.createElement('input');
-    button.classList.add('txt', 'show', 'button');
-    button.setAttribute('type', 'submit');
-    button.setAttribute('value', 'BUY TICKETS');
-    return button;
-};
-
-const renderShows = (arr) => {
-
-    const $desktopDiv = createNewElement('div', 'desktop');
-    const $dateLabel = createNewElement('p', ['txt', 'show', 'label', 'desktop'], 'DATE');
-    const $placeLabel = createNewElement('p', ['txt', 'show', 'label', 'desktop'], 'VENUE');
-    const $locationLabel = createNewElement('p', ['txt', 'show', 'label', 'desktop'], 'LOCATION');
-    $desktopDiv.appendChild($dateLabel);
-    $desktopDiv.appendChild($placeLabel);
-    $desktopDiv.appendChild($locationLabel);
-    $showListEl.appendChild($desktopDiv);
-
-    arr.forEach((show) => {
-        const $showRowEl = createNewElement('div');
-        $showRowEl.setAttribute('data-showrow', '');
-        const $dateLabel = createNewElement('p', ['txt', 'show', 'label', 'mobile'], 'DATE');
-        const $date = createNewElement('time', ['txt', 'show'], convertTimestampToDate(show.date));
-        const $placeLabel = createNewElement('p', ['txt', 'show', 'label', 'mobile'], 'VENUE');
-        const $place = createNewElement('p', ['txt', 'show'], show.place);
-        const $locationLabel = createNewElement('p', ['txt', 'show', 'label', 'mobile'], 'LOCATION');
-        const $location = createNewElement('p', ['txt', 'show'], show.location);
-        const $showButtonEl = createButton();
-        $showRowEl.appendChild($dateLabel);
-        $showRowEl.appendChild($date);
-        $showRowEl.appendChild($placeLabel);
-        $showRowEl.appendChild($place);
-        $showRowEl.appendChild($locationLabel);
-        $showRowEl.appendChild($location);
-        $showRowEl.appendChild($showButtonEl);
-        $showListEl.appendChild($showRowEl);
-    });
-
-};
-
-const convertTimestampToDate = (timestamp) => {
-    const date = timestamp ? new Date(timestamp) : new Date();
-    const formattedDate = date.toDateString();
-    return formattedDate;
-};
-
-
-
-// API DETAILS FOR FETCH REQUEST
-const apiUrl = `https://project-1-api.herokuapp.com`;
-const queryParams = `?api_key=`;
-const apiKey = `1636ce99-0ac4-46f6-b625-cac457fb121f`;
-const routeShowdates = `${apiUrl}/showdates${queryParams}${apiKey}`;
-
-
-async function fetchData(apiUrl) {
-    try {
-        const response = await fetch(apiUrl);
-        if (response.ok) {
-            const data = await response.json();
-            renderShows(data);
+const showdateActionFactory = () => {
+    const createNewElement = (el, classNames, text) => {
+        const $el = document.createElement(el);
+        if (Array.isArray(classNames)) {
+            classNames.forEach(className => $el.classList.add(className));
         } else {
-            console.log('NOT OK!');
+            classNames && ($el.classList.add(classNames));
         }
-    } catch (error) {
-        console.error('Error: ', error);
-        throw error;
-    }
-}
+        text && ($el.textContent = text);
+        return $el;
+    };
 
-function initialLoad(apiUrl = routeShowdates) {
-    fetchData(apiUrl);
-}
+    const createButton = () => {
+        const button = document.createElement('input');
+        button.classList.add('txt', 'show', 'button');
+        button.setAttribute('type', 'submit');
+        button.setAttribute('value', 'BUY TICKETS');
+        return button;
+    };
 
-// renderShows(showArray);
-initialLoad();
+    const convertTimestampToDate = (timestamp) => {
+        const date = timestamp ? new Date(timestamp) : new Date();
+        const formattedDate = date.toDateString();
+        return formattedDate;
+    };
+
+    const renderShows = (arr) => {
+        const $showListEl = document.querySelector('.show-list');
+
+        const $desktopDiv = createNewElement('div', 'desktop');
+        const $dateLabel = createNewElement('p', ['txt', 'show', 'label', 'desktop'], 'DATE');
+        const $placeLabel = createNewElement('p', ['txt', 'show', 'label', 'desktop'], 'VENUE');
+        const $locationLabel = createNewElement('p', ['txt', 'show', 'label', 'desktop'], 'LOCATION');
+        $desktopDiv.appendChild($dateLabel);
+        $desktopDiv.appendChild($placeLabel);
+        $desktopDiv.appendChild($locationLabel);
+        $showListEl.appendChild($desktopDiv);
+
+        arr.forEach((show) => {
+            const $showRowEl = createNewElement('div');
+            $showRowEl.setAttribute('data-showrow', '');
+            const $dateLabel = createNewElement('p', ['txt', 'show', 'label', 'mobile'], 'DATE');
+            const $date = createNewElement('time', ['txt', 'show'], convertTimestampToDate(show.date));
+            const $placeLabel = createNewElement('p', ['txt', 'show', 'label', 'mobile'], 'VENUE');
+            const $place = createNewElement('p', ['txt', 'show'], show.place);
+            const $locationLabel = createNewElement('p', ['txt', 'show', 'label', 'mobile'], 'LOCATION');
+            const $location = createNewElement('p', ['txt', 'show'], show.location);
+            const $showButtonEl = createButton();
+            $showRowEl.appendChild($dateLabel);
+            $showRowEl.appendChild($date);
+            $showRowEl.appendChild($placeLabel);
+            $showRowEl.appendChild($place);
+            $showRowEl.appendChild($locationLabel);
+            $showRowEl.appendChild($location);
+            $showRowEl.appendChild($showButtonEl);
+            $showListEl.appendChild($showRowEl);
+        });
+
+
+    };
+
+    return { renderShows };
+};
+
+const fetchActionFactory = () => {
+    const commentsBaseUrl = `https://project-1-api.herokuapp.com/showdates`;
+    const apiKey = `?api_key=1636ce99-0ac4-46f6-b625-cac457fb121f`;
+    const allShowdatesRoute = `${commentsBaseUrl}${apiKey}`;
+
+    const getShowdates = async () => {
+        let data;
+        try {
+            const response = await fetch(allShowdatesRoute);
+            if (response.ok) {
+                data = await response.json();
+                const domMethods = showdateActionFactory();
+                // console.log(domMethods);
+                domMethods.renderShows(data);
+                // domMethods.renderShows(data);
+            }
+            else { console.log('NOT OK!'); }
+        } catch (error) {
+            console.error('Error fetching comments:', error);
+        }
+        return data;
+    };
+
+
+    return { getShowdates };
+};
+
+const initalLoad = fetchActionFactory();
+initalLoad.getShowdates();
